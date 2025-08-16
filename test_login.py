302c52915b762d1b8c3c327d078f06b5bc4803da
sivaneshMK <sivaneshmk4@gmail.com>
2025-08-16 21:04:51 +0530

    test

--- test_login.py
+++ test_login.py
@@ -5,3 +5,8 @@ def test_login_with_valid_cred():
     driver = webdriver.Chrome()
     driver.get("https://www.facebook.com")
     print(driver.title)
+
+def test_login_with_invalid_cred():
+    driver = webdriver.Chrome()
+    driver.get("https://www.facebook.com")
+    print(driver.title)
